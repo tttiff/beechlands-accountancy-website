dc7f80dae15fb3c233fb16b3529e3cc9b923d7df
Tiffany Charles <charles.tiffany.r@gmail.com>
2021-03-24 14:27:48 +0000

    updated config to fix content length header was 181930, but should be 182216

--- config.rb
+++ config.rb
@@ -20,3 +20,25 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+class ZapContentLength < Struct.new(:app)
+  def call(env)
+    s, h, b = app.call(env)
+    # The URL rewriters in Middleman do not update Content-Length correctly,
+    # which makes Rack-Lint flag the responses as having a wrong Content-Length.
+    # For building assets this has zero importance because the Content-Length
+    # header will be discarded - it is the server that recomputes it. But
+    # it does prevent the site from building correctly.
+    #
+    # The fastest way out of this is to let Rack recompute the Content-Length
+    # forcibly, for every response, at retrieval time.
+    #
+    # See https://github.com/middleman/middleman/issues/2309
+    # and https://github.com/rack/rack/issues/1472
+    h.delete('Content-Length')
+    [s, h, b]
+  end
+end
+
+app.use ::Rack::ContentLength
+app.use ZapContentLength
